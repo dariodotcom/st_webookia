c9689805ae17c94d35ca7e6f47006249cf11aa09
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-18 14:05:05 +0000

    Re-upload model

---
+++ Webookia/src/it/webookia/backend/model/Message.java
@@ -0,0 +1,129 @@
+package it.webookia.backend.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.google.appengine.api.datastore.Key;
+
+import org.slim3.datastore.Attribute;
+import org.slim3.datastore.Model;
+import org.slim3.datastore.ModelRef;
+
+@Model(schemaVersion = 1)
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    // Default constructor
+    public Message() {
+        relativeLoan = new ModelRef<Loan>(Loan.class);
+        author = new ModelRef<UserEntity>(UserEntity.class);
+    }
+
+    @Attribute(primaryKey = true)
+    private Key key;
+
+    @Attribute(version = true)
+    private Long version;
+
+    // Fields
+    private Date date;
+    private String text;
+
+    // Relationship
+    private ModelRef<Loan> relativeLoan;
+    private ModelRef<UserEntity> author;
+
+    // Getters and setters
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ModelRef<Loan> getRelativeLoan() {
+        return relativeLoan;
+    }
+
+    public ModelRef<UserEntity> getAuthor() {
+        return author;
+    }
+
+    /**
+     * Returns the key.
+     * 
+     * @return the key
+     */
+    public Key getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the key.
+     * 
+     * @param key
+     *            the key
+     */
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    /**
+     * Returns the version.
+     * 
+     * @return the version
+     */
+    public Long getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the version.
+     * 
+     * @param version
+     *            the version
+     */
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((key == null) ? 0 : key.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Message other = (Message) obj;
+        if (key == null) {
+            if (other.key != null) {
+                return false;
+            }
+        } else if (!key.equals(other.key)) {
+            return false;
+        }
+        return true;
+    }
+}
