0712cb174242745b95011f0efa33c18a4afbb232
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-18 16:19:06 +0000

    Add Javadoc to Resource Exception

--- Webookia/src/it/webookia/backend/controller/resources/exception/ResourceException.java
+++ Webookia/src/it/webookia/backend/controller/resources/exception/ResourceException.java
@@ -1,30 +1,53 @@
 package it.webookia.backend.controller.resources.exception;
 
+/**
+ * Error that occurs during the elaboration of a request by a resource. It
+ * carries a user friendly explanation plus a cause if the error was triggered
+ * by another exception.
+ */
 public class ResourceException extends Exception {
 
-	private static final long serialVersionUID = 7160398357284715016L;
+    private static final long serialVersionUID = 7160398357284715016L;
 
-	private ResourceErrorType errorType;
+    private ResourceErrorType errorType;
 
-	public ResourceException(ResourceErrorType errorType) {
-		super(errorType.getFriendlyText());
-		this.errorType = errorType;
-	}
+    /**
+     * Constructs a {@link ResourceException} which contains only the error
+     * type.
+     * 
+     * @param errorType
+     *            - the {@link ResourceErrorType} that describes the cause of
+     *            the error.
+     */
+    public ResourceException(ResourceErrorType errorType) {
+        super(errorType.getFriendlyText());
+        this.errorType = errorType;
+    }
 
-	public ResourceException(ResourceErrorType errorType, Throwable cause) {
-		super(errorType.getFriendlyText(), cause);
-		this.errorType = errorType;
-	}
+    /**
+     * Constructs a {@link ResourceException} which contains the description of
+     * the cause and the {@link Throwable} that triggered the error.
+     * 
+     * @param errorType
+     *            - the {@link ResourceErrorType} that describes the cause of
+     *            the error.
+     * @param cause
+     *            - the {@link Throwable} that triggered the error.
+     */
+    public ResourceException(ResourceErrorType errorType, Throwable cause) {
+        super(errorType.getFriendlyText(), cause);
+        this.errorType = errorType;
+    }
 
-	@Override
-	public String toString() {
-		String className = getClass().getName();
-		String message = getLocalizedMessage();
-		String result = className + " [" + errorType.getErrorName() + "]";
-		return (message != null) ? (result + ": " + message) : result;
-	}
+    @Override
+    public String toString() {
+        String className = getClass().getName();
+        String message = getLocalizedMessage();
+        String result = className + " [" + errorType.getErrorName() + "]";
+        return (message != null) ? (result + ": " + message) : result;
+    }
 
-	public ResourceErrorType getErrorType() {
-		return errorType;
-	}
+    public ResourceErrorType getErrorType() {
+        return errorType;
+    }
 }
