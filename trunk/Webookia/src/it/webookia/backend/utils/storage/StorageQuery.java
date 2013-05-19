ee13489e6931e3e71147a9a7163ebe5aa0d47f69
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-19 09:36:48 +0000

    Add query to retrieve user friends.

--- trunk/Webookia/src/it/webookia/backend/utils/storage/StorageQuery.java
+++ trunk/Webookia/src/it/webookia/backend/utils/storage/StorageQuery.java
@@ -1,9 +1,13 @@
 package it.webookia.backend.utils.storage;
 
+import java.util.List;
+
 import org.slim3.datastore.Datastore;
 
 import it.webookia.backend.meta.DetailedBookMeta;
+import it.webookia.backend.meta.UserEntityMeta;
 import it.webookia.backend.model.DetailedBook;
+import it.webookia.backend.model.UserEntity;
 
 public class StorageQuery {
 
@@ -12,4 +16,16 @@ public class StorageQuery {
         return Datastore.query(book).filter(book.isbn.equal(isbn)).asSingle();
     }
 
+    public static UserEntity getUserByUsername(String username) {
+        UserEntityMeta user = UserEntityMeta.get();
+        return Datastore
+            .query(user)
+            .filter(user.userId.equal(username))
+            .asSingle();
+    }
+
+    public static List<UserEntity> getFriends(List<String> friendsID) {
+        UserEntityMeta user = UserEntityMeta.get();
+        return Datastore.query(user).filter(user.userId.in(friendsID)).asList();
+    }
 }
