3c2ce9ec26a79a6b3ec3d06d913c79de83a34d5b
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-19 12:29:22 +0000

    Add Feedback model

---
+++ trunk/Webookia/src/it/webookia/backend/model/Feedback.java
@@ -0,0 +1,130 @@
+package it.webookia.backend.model;
+
+import it.webookia.backend.utils.storage.Storable;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import org.slim3.datastore.Attribute;
+import org.slim3.datastore.Model;
+
+@Model(schemaVersion = 1)
+public class Feedback implements Serializable, Storable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Attribute(primaryKey = true)
+    private Key key;
+
+    @Attribute(version = true)
+    private Long version;
+
+    @Override
+    public String getId() {
+        return KeyFactory.keyToString(key);
+    }
+
+    private int mark;
+    private Date date;
+    private String text;
+
+    // Getters and setters
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    /**
+     * Returns the key.
+     * 
+     * @return the key
+     */
+    public Key getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the key.
+     * 
+     * @param key
+     *            the key
+     */
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    /**
+     * Returns the version.
+     * 
+     * @return the version
+     */
+    public Long getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the version.
+     * 
+     * @param version
+     *            the version
+     */
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((key == null) ? 0 : key.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Feedback other = (Feedback) obj;
+        if (key == null) {
+            if (other.key != null) {
+                return false;
+            }
+        } else if (!key.equals(other.key)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+}
