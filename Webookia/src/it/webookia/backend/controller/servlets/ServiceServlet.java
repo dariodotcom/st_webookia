85a2b215d880c5ef4402688287d048d9be838c3a
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-21 21:34:12 +0000

    Complete implementation of servlet service

--- Webookia/src/it/webookia/backend/controller/servlets/ServiceServlet.java
+++ Webookia/src/it/webookia/backend/controller/servlets/ServiceServlet.java
@@ -9,50 +9,71 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ServiceServlet extends HttpServlet{
+public class ServiceServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -6364544534456443497L;
 
     private Map<String, Service> getServices;
     private Map<String, Service> postServices;
-    private String sectionName;
-    
-    protected ServiceServlet(String section){
+    private String contextName;
+
+    protected ServiceServlet(String section) {
         this.getServices = new HashMap<String, Service>();
         this.postServices = new HashMap<String, Service>();
-        this.sectionName = section;
+        this.contextName = section;
     }
-    
-    protected void registerGetService(String action, Service service){
+
+    protected final void registerGetService(String action, Service service) {
         this.getServices.put(action, service);
     }
-    
-    protected void registerPostService(String action, Service service){
+
+    protected final void registerPostService(String action, Service service) {
         this.postServices.put(action, service);
     }
 
     @Override
     protected final void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        
-        
-        
+        String actionName = getAction(req);
+        if (!getServices.keySet().contains(actionName)) {
+            throw new ServletException(String.format(
+                "No GET handler for action %s in context %s",
+                actionName,
+                contextName));
+        }
+
+        getServices.get(actionName).service(req, resp);
     }
 
     @Override
     protected final void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doPost(req, resp);
-    }
-    
-    private String getAction(HttpServletRequest request) throws ServletException{
-        String requestAction = request.getRequestURI().replace(request.getContextPath(), "");
-        String[] components = requestAction.split("/");
-        
-        if(!components[0].equals(this.sectionName)){
-            
+        String actionName = getAction(req);
+        if (!postServices.keySet().contains(actionName)) {
+            throw new ServletException(String.format(
+                "No POST handler for action %s in context %s",
+                actionName,
+                contextName));
         }
-        
+
+        postServices.get(actionName).service(req, resp);
     }
-    
-    
+
+    private String getAction(HttpServletRequest request)
+            throws ServletException {
+        String requestAction =
+            request.getRequestURI().replace(request.getContextPath(), "");
+        String[] components = requestAction.split("/");
+
+        if (!components[0].equals(this.contextName)) {
+            String message = "Service context error. Managed: %s, received: %s";
+            throw new ServletException(String.format(
+                message,
+                components[0],
+                this.contextName));
+        }
+
+        return components[1];
+    }
+
 }
