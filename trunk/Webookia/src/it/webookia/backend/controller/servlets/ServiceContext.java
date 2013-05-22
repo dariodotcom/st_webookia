637865bd67c4f104251cb97b6221280142a3debe
ThinkyClaire <ThinkyClaire@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-22 15:48:52 +0000

    Authentication

--- trunk/Webookia/src/it/webookia/backend/controller/servlets/ServiceContext.java
+++ trunk/Webookia/src/it/webookia/backend/controller/servlets/ServiceContext.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ServiceContext {
 
+    private static final String loggedUserAttribute = "LOGGED_ATTR";
+
     private HttpServletRequest request;
     private HttpServletResponse response;
 
@@ -13,6 +15,30 @@ public class ServiceContext {
         super();
         this.request = request;
         this.response = response;
+    }
+
+    public String getAuthenticatedUserId() {
+        return (String) request.getSession().getAttribute(loggedUserAttribute);
+    }
+
+    public void setLoggedUserId(String userID) {
+        request.getSession().setAttribute(loggedUserAttribute, userID);
+    }
+
+    public Object getAttribute(String arg0) {
+        return request.getAttribute(arg0);
+    }
+
+    public String getContextPath() {
+        return request.getContextPath();
+    }
+
+    public String getParameter(String arg0) {
+        return request.getParameter(arg0);
+    }
+
+    public void setAttribute(String arg0, Object arg1) {
+        request.setAttribute(arg0, arg1);
     }
 
     // Standard getters
---
+++ trunk/Webookia/src/it/webookia/backend/utils/foreignws/facebook/OAuthException.java
@@ -0,0 +1,15 @@
+package it.webookia.backend.utils.foreignws.facebook;
+
+public class OAuthException extends Exception {
+
+    private static final long serialVersionUID = -2115419977832811335L;
+
+    public OAuthException() {
+        super();
+    }
+
+    public OAuthException(String message) {
+        super(message);
+    }
+    
+}
