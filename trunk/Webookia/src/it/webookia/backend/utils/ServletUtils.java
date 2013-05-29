72debcc18fab45eff699356d0ec5e746e234f9c9
claudia.colella90 <claudia.colella90@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-05-29 07:21:36 +0000

    Rename username to userid in servletUtils

--- trunk/Webookia/src/it/webookia/backend/utils/ServletUtils.java
+++ trunk/Webookia/src/it/webookia/backend/utils/ServletUtils.java
@@ -6,11 +6,11 @@ public class ServletUtils {
 
     private static final String AUTH_USER = "AUTH_USER";
 
-    public static String getAuthenticatedUsername(HttpServletRequest req) {
+    public static String getAuthenticatedUserId(HttpServletRequest req) {
         return (String) req.getSession().getAttribute(AUTH_USER);
     }
 
-    public static void setAuthenticatedUsername(HttpServletRequest req,
+    public static void setAuthenticatedUserId(HttpServletRequest req,
             String id) {
         req.getSession().setAttribute(AUTH_USER, id);
     }
