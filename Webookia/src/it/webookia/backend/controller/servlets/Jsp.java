b9a64540c7cef33ff373b428e1425691d072c04a
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-03 19:53:54 +0000

    Implement first part of html interface

---
+++ Webookia/src/it/webookia/backend/controller/servlets/Jsp.java
@@ -0,0 +1,16 @@
+package it.webookia.backend.controller.servlets;
+
+public enum Jsp {
+
+    HOME("/home.jsp");
+
+    private String url;
+
+    private Jsp(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
