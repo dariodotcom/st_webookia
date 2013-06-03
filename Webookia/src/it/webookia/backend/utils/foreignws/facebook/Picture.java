20fc4769df968b2d6e9c78609559f87c6e206923
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-03 18:06:55 +0000

    Add method to retrieve customer picture url from facebook connector

---
+++ Webookia/src/it/webookia/backend/utils/foreignws/facebook/Picture.java
@@ -0,0 +1,42 @@
+package it.webookia.backend.utils.foreignws.facebook;
+
+import com.restfb.Facebook;
+import com.restfb.types.NamedFacebookType;
+
+public class Picture extends NamedFacebookType {
+
+    private static final long serialVersionUID = -2854932488997969939L;
+
+    @Facebook("data")
+    private ImageData data;
+
+    public static class ImageData {
+        @Facebook
+        private String url;
+
+        @Facebook
+        private int width;
+
+        @Facebook
+        private int height;
+
+        @Facebook("is_silhouette")
+        private boolean silhouette;
+    }
+
+    public String getUrl() {
+        return data.url;
+    }
+
+    public int getWidth() {
+        return data.width;
+    }
+
+    public int getHeight() {
+        return data.height;
+    }
+
+    public boolean isSilhouette() {
+        return data.silhouette;
+    }
+}
