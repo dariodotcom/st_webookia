8d971076b8599fb4b5bca671fd201d4356031709
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-10 19:52:58 +0000

    Remove not needed methods.

--- Webookia/src/it/webookia/backend/utils/CollectionUtils.java
+++ Webookia/src/it/webookia/backend/utils/CollectionUtils.java
@@ -1,6 +1,7 @@
 package it.webookia.backend.utils;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class CollectionUtils {
@@ -11,6 +12,20 @@ public class CollectionUtils {
             output.add(mapper.map(inputElem));
         }
         return output;
+    }
+
+    public static <E extends Object> String join(List<E> input, String separator) {
+        StringBuilder result = new StringBuilder();
+        Iterator<E> it = input.iterator();
+
+        while (it.hasNext()) {
+            result.append(it.next());
+            if (separator != null && it.hasNext()) {
+                result.append(separator);
+            }
+        }
+
+        return result.toString();
     }
 
 }
