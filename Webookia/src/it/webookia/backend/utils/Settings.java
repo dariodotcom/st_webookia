7552338f97851941738fe389c3b34b470d8f7a70
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-10 19:52:21 +0000

    Add global date formatter

--- Webookia/src/it/webookia/backend/utils/Settings.java
+++ Webookia/src/it/webookia/backend/utils/Settings.java
@@ -1,5 +1,13 @@
 package it.webookia.backend.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Settings {
-    public final static String CURRENT_HOST = "http://87.5.61.78:8888";
+    public final static String CURRENT_HOST = "http://87.8.47.42:8888";
+
+    public final static int LOAN_PAGINATION_SIZE = 15;
+
+    public final static DateFormat DATE_FORMAT = new SimpleDateFormat(
+        "dd/MM/yyyy HH:mm");
 }
