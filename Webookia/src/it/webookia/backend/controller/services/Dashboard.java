9fbf238afd25da610169c6b8bb4e4665a4f5bc16
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-10 20:02:13 +0000

    Update services

--- Webookia/src/it/webookia/backend/controller/services/Dashboard.java
+++ Webookia/src/it/webookia/backend/controller/services/Dashboard.java
@@ -1,5 +1,14 @@
 package it.webookia.backend.controller.services;
 
-public class Dashboard {
+import it.webookia.backend.controller.services.impl.ServiceServlet;
+
+public class Dashboard extends ServiceServlet {
+
+    private static final long serialVersionUID = -4989089940525593983L;
+
+    protected Dashboard() {
+        super("dashboard");
+        // TODO Auto-generated constructor stub
+    }
 
 }
--- Webookia/src/it/webookia/backend/controller/services/Loans.java
+++ Webookia/src/it/webookia/backend/controller/services/Loans.java
@@ -8,6 +8,7 @@ import it.webookia.backend.controller.resources.BookResource;
 import it.webookia.backend.controller.resources.LoanResource;
 import it.webookia.backend.controller.resources.UserResource;
 import it.webookia.backend.controller.resources.exception.ResourceException;
+import it.webookia.backend.controller.services.impl.Jsp;
 import it.webookia.backend.controller.services.impl.Service;
 import it.webookia.backend.controller.services.impl.ServiceContext;
 import it.webookia.backend.controller.services.impl.ServiceServlet;
@@ -17,34 +18,79 @@ public class Loans extends ServiceServlet {
 
     private static final long serialVersionUID = 2335276930451134433L;
 
+    public static final String CONTEXT_LOAN_ID = "id";
+    public static final String CONTEXT_LOAN = "CONTEXT_LOAN";
+    public static final String SENT_LOANS = "SENT_LOANS";
+    public static final String RECEIVED_LOANS = "RECEIVED_LOANS";
+
     public Loans() {
-        super("loan");
+        super("loans");
+        registerDefaultService(Verb.GET, new LoanLanding());
         registerService(Verb.POST, "create", new LoanCreation());
+        registerService(Verb.GET, "detail", new LoanDetail());
     }
 
     public class LoanCreation implements Service {
-
         @Override
         public void service(ServiceContext context) throws ServletException,
                 IOException {
-            
-            String bookId = context.getConcreteBookId();
+
+            String bookId = null;
             String userId = context.getAuthenticatedUserId();
-            
+
             try {
                 UserResource requestor = UserResource.getUser(userId);
                 BookResource book = BookResource.getBook(bookId, requestor);
                 LoanResource loan = LoanResource.createLoan(requestor, book);
                 String loanId = loan.getDescriptor().getId();
-                String newUrl = "/loan/detail?id="+loanId;
+                String newUrl = "/loan/detail?id=" + loanId;
                 context.sendRedirect(newUrl);
             } catch (ResourceException e) {
                 context.sendError(e);
                 return;
             }
+        }
+    }
+
+    public class LoanLanding implements Service {
+
+        @Override
+        public void service(ServiceContext context) throws ServletException,
+                IOException {
+            String userId = context.getAuthenticatedUserId();
+
+            try {
+                UserResource user = UserResource.getUser(userId);
+                context.setRequestAttribute(SENT_LOANS, user
+                    .getSentLoanRequest(-1)
+                    .getList());
+                context.setRequestAttribute(RECEIVED_LOANS, user
+                    .getReceivedLoanRequest(-1)
+                    .getList());
+                context.forwardToJsp(Jsp.LOAN_JSP);
+            } catch (ResourceException e) {
+                context.sendError(e);
+            }
 
         }
+    }
 
+    public class LoanDetail implements Service {
+
+        @Override
+        public void service(ServiceContext context) throws ServletException,
+                IOException {
+            String loanId = context.getRequestParameter(CONTEXT_LOAN_ID);
+            String requestorId = context.getAuthenticatedUserId();
+            try {
+                UserResource requestor = UserResource.getUser(requestorId);
+                LoanResource loan = LoanResource.getLoan(requestor, loanId);
+                context.setRequestAttribute(CONTEXT_LOAN, loan);
+                context.forwardToJsp(Jsp.LOAN_DETAIL);
+            } catch (ResourceException e) {
+                context.sendError(e);
+            }
+        }
     }
 
     // richiesta prestito
