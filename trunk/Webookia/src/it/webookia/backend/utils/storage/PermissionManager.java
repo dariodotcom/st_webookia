2533f590fd9f6354991a04dd367d48bc0e46cf55
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-15 16:13:05 +0000

    Implement permission manager

---
+++ trunk/Webookia/src/it/webookia/backend/utils/storage/PermissionManager.java
@@ -0,0 +1,185 @@
+package it.webookia.backend.utils.storage;
+
+import it.webookia.backend.enums.BookStatus;
+import it.webookia.backend.enums.LoanStatus;
+import it.webookia.backend.enums.PrivacyLevel;
+import it.webookia.backend.model.ConcreteBook;
+import it.webookia.backend.model.DetailedBook;
+import it.webookia.backend.model.Loan;
+import it.webookia.backend.model.UserEntity;
+import it.webookia.backend.utils.DebugLogger;
+
+public class PermissionManager {
+
+    private static DebugLogger logger = new DebugLogger("Permission Manager");;
+
+    /**
+     * Creates a new {@link PermissionManager} for given user.
+     * 
+     * @param user
+     *            - the {@link UserEntity} whose permission we want to check
+     * @return
+     */
+    public static PermissionManager user(UserEntity userEntity) {
+        return new PermissionManager(userEntity);
+    }
+
+    private UserEntity user;
+
+    private PermissionManager(UserEntity user) {
+        this.user = user;
+    }
+
+    /**
+     * Checks if an user can access a concrete book. It's the case if: - the
+     * book is public, or - the book is for friends only and the user is friend
+     * with book owner.
+     * 
+     * @param concreteBook
+     * @return true if the user can access the book.
+     */
+    public boolean canAccess(ConcreteBook concreteBook) {
+        PrivacyLevel privacy = concreteBook.getPrivacy();
+        UserEntity owner = concreteBook.getOwner();
+
+        if (privacy.equals(PrivacyLevel.PUBLIC)) {
+            return true;
+        } else if (user == null) {
+            return false;
+        } else if (user.equals(owner)) {
+            return true;
+        } else {
+            return false; // TODO Check friendship
+        }
+    }
+
+    /**
+     * Checks if user can access given loan. It's the case if he is either the
+     * borrower or the lent book owner.
+     * 
+     * @param loan
+     *            - the {@link Loan}
+     * @return - true if the user can access the loan
+     */
+    public boolean canAccess(Loan loan) {
+        UserEntity owner = loan.getLentBook().getOwner();
+        UserEntity borrower = loan.getBorrower();
+
+        return user.equals(owner) || user.equals(borrower);
+    }
+
+    /**
+     * Checks if the user can share a book. It's the case if he doesn't have
+     * that book shared already.
+     * 
+     * @param detailedBook
+     *            - the {@link DetailedBook} to share.
+     * @return true if the user can share it.
+     */
+    public boolean canShare(DetailedBook detailedBook) {
+        for (ConcreteBook book : user.getOwnedBooks()) {
+            if (book.getDetailedBook().equals(detailedBook)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the user can borrow a given book. It's the case if the book is
+     * accessible by the user, he's not the owner and the book is available.
+     * 
+     * @param concreteBook
+     *            - the book to borrow.
+     * @return
+     */
+    public boolean canBorrow(ConcreteBook concreteBook) {
+        if (user == null) {
+            logger.log("Non logged in user cannot borrow books");
+            return false;
+        }
+
+        if (!canAccess(concreteBook)) {
+            return false;
+        }
+
+        if (concreteBook.getOwner().equals(user)) {
+            logger.log("User "
+                + user.getUserId()
+                + " cannot borrow his own book");
+        }
+
+        if (concreteBook.getStatus() != BookStatus.AVAILABLE) {
+            logger.log("Book " + concreteBook.getId() + "is not available");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks if an user can review a book. It's the case only if he is the
+     * owner and he hasn't already inserted a review.
+     * 
+     * @param book
+     *            - the {@link ConcreteBook} to review.
+     * @return true if the user is allowed.
+     */
+    public boolean canReview(ConcreteBook book) {
+        if (!book.getOwner().equals(user) || book.getReview() != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks if an user can comment on a review of a book. It's the case only
+     * if he can access the book and the book has a review.
+     * 
+     * @param book
+     *            - the {@link ConcreteBook} to review.
+     * @return true if the user is allowed.
+     */
+    public boolean canCommentOnReview(ConcreteBook book) {
+        if (!canAccess(book) || book.getReview() == null) {
+            return false;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if an user can send a message in a loan. It's the case only if he
+     * is involved and the loan.
+     * 
+     * @param loan
+     *            - the loan in which to send the message.
+     * @return true if the user is allowed.
+     */
+    public boolean canSendMessage(Loan loan) {
+        if (!canAccess(loan)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks if an user can send a feedback in a loan. It's the case only if he
+     * is involved and the loan is in status GIVEN_BACK.
+     * 
+     * @param loan
+     *            - the loan in which to send the feedback.
+     * @return true if the user is allowed.
+     */
+    public boolean canSendFeedback(Loan loan) {
+        if (!canAccess(loan) || !loan.getStatus().equals(LoanStatus.GIVEN_BACK)) {
+            return false;
+        }
+
+        return false;
+    }
+
+}
