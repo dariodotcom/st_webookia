ccbedca4325ddab30dd197ecd00b2f3bc631ef50
dariodotcom <dariodotcom@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-06-18 15:03:06 +0000

    Implement error messages.

--- Webookia/war/js/Webookia.Core.js
+++ Webookia/war/js/Webookia.Core.js
@@ -21,119 +21,149 @@ var $doc = document.getElementById.bind(document);
 
 	// Initializer
 	var initializers = new Array();
-	
+
 	var Initializer = Webookia.Initializer = {
-		plugin: function _plugin(initializer){
+		plugin : function _plugin(initializer) {
 			initializers.push(initializer);
 		}
 	}
-	
-	$(function(){
-		$(initializers).each(function(index, f){
+
+	$(function() {
+		$(initializers).each(function(index, f) {
 			f.call(window);
 		})
 	})
 
+	// Error
+	var errorContainer;
+	Webookia.Initializer.plugin(function() {
+		errorContainer = $doc("errorContainer")
+				|| ($("<div id=\"errorContainer\"></div>")
+						.prependTo($("#content")));
+		
+		//Test
+		Webookia.Error.append("Non puoi eseguire l'azione richiesta.");
+	});
+
+	var Error = Webookia.Error = {
+		append : function _append(errorMsg) {
+			var error = $("<div class=\"error clearfix\"></div>");
+			$("<div class=\"errorText\"></div>").text(errorMsg).appendTo(error);
+			var close = $("<div class=\"close\"></div>").text("x");
+			close.appendTo(error);
+			close.click(function() {
+				error.remove();
+			});
+			error.appendTo(errorContainer);
+		}
+
+	}
+
 	// Concrete UI
-	function toCoords(elem){
+	function toCoords(elem) {
 		var coords = elem.text();
-		if(coords == ""){
+		if (coords == "") {
 			return null;
 		}
-		
-	    var split = coords.split(",");
-	    return new google.maps.LatLng(parseFloat(split[0]), parseFloat(split[1]));
+
+		var split = coords.split(",");
+		return new google.maps.LatLng(parseFloat(split[0]),
+				parseFloat(split[1]));
 	}
-	
+
 	var ConcreteUI = {
-		map: null,
-		concretes: null,
-		popup: null,
-		
-		loadMap: function _loadMap(){
+		map : null,
+		concretes : null,
+		popup : null,
+
+		loadMap : function _loadMap() {
 			var mapOptions = {
 				zoom : 8,
 				center : toCoords($('.selfCoords')),
 				mapTypeId : google.maps.MapTypeId.ROADMAP
 			};
-			
+
 			this.map = new google.maps.Map($doc('map'), mapOptions);
 		},
-		
-		bounceMarker: function _bounceMarker(marker){
-		    var position = marker.getPosition();
-		    this.map.panTo(position);
-		    marker.setAnimation(google.maps.Animation.BOUNCE);
-		    setTimeout(function(){marker.setAnimation(null)}, 2000);
+
+		bounceMarker : function _bounceMarker(marker) {
+			var position = marker.getPosition();
+			this.map.panTo(position);
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function() {
+				marker.setAnimation(null)
+			}, 2000);
 		},
-		
-		loadPopup: function _loadPopup(){
+
+		loadPopup : function _loadPopup() {
 			var popupElem = $(".concretePopup");
 			popupElem.hide();
 
 			this.popup = {
-			    setName: function(name){
-			        popupElem.children(".text").children(".name").text(name);
-			    },
-			    show: function(){
-			        popupElem.show();
-			    },
-			    hide: function(){
-			        popupElem.hide();
-			    },
-			    top: function(top){
-			        popupElem.css("top", top + "px");
-			    },
+				setName : function(name) {
+					popupElem.children(".text").children(".name").text(name);
+				},
+				show : function() {
+					popupElem.show();
+				},
+				hide : function() {
+					popupElem.hide();
+				},
+				top : function(top) {
+					popupElem.css("top", top + "px");
+				},
 			}
-			
+
 			var buttons = popupElem.children('.buttons');
 			var no = buttons.children('.no');
 			no.click(this.popup.hide);
 		},
-		
-		loadConcretes: function loadConcretes(){
-			this.concretes = $('#concreteList > .concrete');
-			Utility.log("lol");		
-			
-			var ui = this;
-			this.concretes.each(function(index, elem){
-				var concrete = $(elem);
-				var position = toCoords(concrete.children('.concreteDetail').children('.coords'));
-				
-			    var marker = new google.maps.Marker({
-			    	position: position,
-			    	map: ui.map
-			    });
 
-			    concrete.click(ui.selectConcrete.bind(ui, concrete, marker));
+		loadConcretes : function loadConcretes() {
+			this.concretes = $('#concreteList > .concrete');
+			Utility.log("lol");
+
+			var ui = this;
+			this.concretes.each(function(index, elem) {
+				var concrete = $(elem);
+				var position = toCoords(concrete.children('.concreteDetail')
+						.children('.coords'));
+
+				var marker = new google.maps.Marker({
+					position : position,
+					map : ui.map
+				});
+
+				concrete.click(ui.selectConcrete.bind(ui, concrete, marker));
 			});
 		},
-		
-		selectConcrete: function _selectConcrete(concrete, marker){
-		    this.popup.hide();
-		    
-		    var top = concrete.position().top;
-		    var height = concrete.height();
-		    var newPosition = top + height + 9;
-		    Utility.log("Setting height to " + (top + height));
-		    this.popup.top(newPosition);
-		    
-		    var name = concrete.children('.concreteDetail').children('.name').text();
-		    this.popup.setName(name);
-		    
-		    this.popup.show();
-		    this.bounceMarker(marker);
+
+		selectConcrete : function _selectConcrete(concrete, marker) {
+			this.popup.hide();
+
+			var top = concrete.position().top;
+			var height = concrete.height();
+			var newPosition = top + height + 9;
+			Utility.log("Setting height to " + (top + height));
+			this.popup.top(newPosition);
+
+			var name = concrete.children('.concreteDetail').children('.name')
+					.text();
+			this.popup.setName(name);
+
+			this.popup.show();
+			this.bounceMarker(marker);
 		},
-		
-		init: function _init(){
+
+		init : function _init() {
 			this.loadMap();
 			this.loadPopup();
 			this.loadConcretes();
-		}	
+		}
 	}
-	
-	Initializer.plugin(function __loadConcreteUI__(){
+
+	Initializer.plugin(function __loadConcreteUI__() {
 		ConcreteUI.init();
 	});
-	
+
 })();
