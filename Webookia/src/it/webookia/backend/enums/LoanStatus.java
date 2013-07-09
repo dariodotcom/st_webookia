85801200209b6321d9c2a8a265bbb93ef6917050
ThinkyClaire <ThinkyClaire@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-07-09 10:53:21 +0000

    Add javadoc

--- Webookia/src/it/webookia/backend/enums/LoanStatus.java
+++ Webookia/src/it/webookia/backend/enums/LoanStatus.java
@@ -1,8 +1,20 @@
 package it.webookia.backend.enums;
 
+import it.webookia.backend.model.Loan;
+
+/**
+ * Enumeration of possible {@link Loan} statuses.
+ */
 public enum LoanStatus {
     INITIAL, ACCEPTED, SHIPPED, GIVEN_BACK, REFUSED;
 
+    /**
+     * Gives the {@link LoanStatus} with given index
+     * 
+     * @param i
+     *            - the index
+     * @return - the {@link LoanStatus} at index i.
+     */
     public static LoanStatus valueOf(int i) {
         if (i >= values().length) {
             throw new IndexOutOfBoundsException();
@@ -11,6 +23,14 @@ public enum LoanStatus {
         return values()[i];
     }
 
+    /**
+     * Retrieves the index of a given status in the array of {@link LoanStatus}
+     * values.
+     * 
+     * @param status
+     *            - the status
+     * @return the index
+     */
     public static int indexOf(LoanStatus status) {
         LoanStatus[] values = values();
         for (int i = 0; i < values.length; i++) {
@@ -19,6 +39,6 @@ public enum LoanStatus {
             }
         }
 
-        return -1;
+        return -1; // This should never happen
     }
 }
