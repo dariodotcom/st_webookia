b8aa16ff89ee7e79720f1f5a45607fcd2c44c09a
ThinkyClaire <ThinkyClaire@01cb4432-3c87-43f4-b97c-7c9abf3460fe>
2013-07-17 07:43:05 +0000

    Add javadoc

--- trunk/Webookia/src/it/webookia/backend/utils/foreignws/facebook/FacebookConnectorException.java
+++ trunk/Webookia/src/it/webookia/backend/utils/foreignws/facebook/FacebookConnectorException.java
@@ -3,17 +3,24 @@ package it.webookia.backend.utils.foreignws.facebook;
 /**
  * Exception that represents errors that may happen while connecting to
  * Facebook.
- * 
- * 
  */
 public class FacebookConnectorException extends Exception {
 
     private static final long serialVersionUID = -2115419977832811335L;
 
+    /**
+     * Class constructor
+     */
     public FacebookConnectorException() {
         super();
     }
 
+    /**
+     * Class constructor
+     * 
+     * @param message
+     *            a specific message of error
+     */
     public FacebookConnectorException(String message) {
         super(message);
     }
